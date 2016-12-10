8d9f81fa9820ff698a2b1de1f980f8139fc4d707
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-09 20:00:11 -0800

    Regex testing

--- regex.py
+++ regex.py
@@ -4,10 +4,11 @@ text_file = open("Main Corpus.txt", "r")
 
 def main(text):
 
-    pretty_text = re.sub(r'[^\w]', ' ', text.read())
+    pretty_text = re.sub(r'[^\w\s]', ' ', text.read())
+    print pretty_text
 
-    '''editted_corpus = open("Main Corpus.txt", "w")
-    editted_corpus.write(processed_result)'''
+    editted_corpus = open("Main Corpus.txt", "w")
+    editted_corpus.write(pretty_text)
 
 if __name__ == "__main__":
     main(text_file)
