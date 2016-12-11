f976745456039690d020d704c93f3e0a62fdcc9f
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-11 12:18:37 -0800

    Regex Progression
    Deleted “Chapter” strings

--- regex.py
+++ regex.py
@@ -4,8 +4,9 @@ text_file = open("Main Corpus.txt", "r")
 
 def main(text):
 
-    pretty_text = re.sub(r'[^\w\s]', ' ', text.read())
-    print pretty_text
+    delete_chapter_regex = 'C\w*\s\d\.\s+'
+
+    pretty_text = re.sub(delete_chapter_regex, '', text.read())
 
     editted_corpus = open("Main Corpus.txt", "w")
     editted_corpus.write(pretty_text)
