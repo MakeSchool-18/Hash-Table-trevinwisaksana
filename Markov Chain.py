e59e72130af88160c6603e9ab35ab0a66e3eda76
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-14 14:57:30 -0800

    Commented Markov Chain

--- Markov Chain.py
+++ Markov Chain.py
@@ -4,51 +4,48 @@ import random
 class Markov(object):
 
     def __init__(self, open_file, chain_size=3):
-        self.chain_size = chain_size
+        self.chain_size = chain_size  # How many words are in the key
         self.cache = {}
         self.open_file = open_file.read()
         self.words = self.open_file.split()
         self.word_size = len(self.words)
-        self.database()
+        self.database()  # Generates a list of key value pairs
 
     def database(self):
-        for chain_set in self.chains():
-            # print(type(chain_set)) # Type Tuple
-            # print(type(self.chains())) # Type Generator
-            key = chain_set[:self.chain_size - 1]
-            next_word = chain_set[-1]  # Last item in the array
-            if key in self.cache:
-                self.cache[key].append(next_word)
+        # chain_set is Type Tuple
+        for chain_set in self.chains():  # self.chains() is Type Generator as it returns a generator
+            key = chain_set[:self.chain_size - 1]  # A tuple that contains the current word and the word next to it
+            next_word = chain_set[-1]  # Last item in the the chain_set or the next word
+            if key in self.cache:  # If the key is already in the dictionary
+                self.cache[key].append(next_word)  # Appends the next word to the list
+                # self.cache contains ('for', 'it'): ['is']
             else:
-                self.cache[key] = [next_word]
+                self.cache[key] = [next_word]  # If it doesn't exist, set the key to the next word
 
     def chains(self):
-        """Generates chains from the given data string based on passed chain size."""
-        for i in range(len(self.words) - self.chain_size - 1):
-            yield tuple(self.words_at_position(i))
+        """Generates chains from the given data string based on passed chain size.
+        ('since.', 'He', 'was')
+        (He', 'was', 'charming.')
+        """
+        for i in xrange(len(self.words) - self.chain_size - 1):  # Loops 461743 times
+            yield tuple(self.words_at_position(i))  # Uses a generator that creates the example on top
 
     def words_at_position(self, i):
         """Uses the chain size to find a list of the words at an index."""
-        chain = []
-        for chain_index in xrange(0, self.chain_size):
-            chain.append(self.words[i + chain_index])
-            # print self.words[i + chain_index]
+        chain = []  # Contains the list of words
+        for chain_index in xrange(0, self.chain_size):  # Iterates from 0-2
+            chain.append(self.words[i + chain_index])  # Appends the current word and word after
         return chain
 
     def generate_markov_text(self, size=15):
-        seed = random.randint(0, self.word_size - 3)  # Get random number
-        # print(self.words_at_position(1))
-        gen_words = []
-        seed_words = self.words_at_position(seed)[:-1]  # Everything on list except for the last one
-        gen_words.extend(seed_words)  # Merges the two lists together
+        seed = random.randint(0, self.word_size - 3)  # Get random number based on word size
+        gen_words = []  # Contains the list of words
+        seed_words = self.words_at_position(seed)[:-1]  # Chooses a random key and takes everything but the last word
+        gen_words.extend(seed_words)  # Merges the seed_words and gen_words together
         for i in xrange(size):  # xrange basically uses yield, which makes it more efficient
-            last_word_len = self.chain_size - 1
-            # print(last_word_len)
-            last_words = gen_words[-1 * last_word_len:] # Always 2
-            # print(type(gen_words))
-            next_word = random.choice(self.cache[tuple(last_words)])
-            # print(next_word)
-            # print(tuple(last_words))
+            last_word_len = self.chain_size - 1  # Always 2
+            last_words = gen_words[-1 * last_word_len:]  # Takes the second from last word
+            next_word = random.choice(self.cache[tuple(last_words)])  # Uses the last words as a key
             gen_words.append(next_word)
         return ' '.join(gen_words)
 
