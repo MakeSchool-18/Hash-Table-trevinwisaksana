ad6138823158702acc78731bd670376c35b99ca0
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-23 14:49:08 -0800

    Second Markov
    – Better version of the Markov Model

---
+++ Second_Markov_Chain.py
@@ -0,0 +1,12 @@
+import random
+
+
+"""
+– Use the 'one fist two fish blue fish red fish'
+– Get a list of key, value pairs.
+    – The key contains the word, the value contains a histogram or word count
+"""
+
+class Markov_Model(object):
+
+    def database():  # Takes
